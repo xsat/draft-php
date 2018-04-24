a89bd1e67da081bb2d5c184d9c9689bce9643454
Xsat <xsat.php@gmail.com>
2018-04-24 21:34:12 +0300

    Rename main class

---
+++ src/DraftHtml.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Draft;
+
+/**
+ * Class DraftHtmlDraftHtml
+ *
+ * @package Draft
+ */
+class DraftHtml
+{
+    /**
+     * @param string $html
+     *
+     * @return array
+     */
+    public function convert(string $html): array
+    {
+        return [];
+    }
+}
